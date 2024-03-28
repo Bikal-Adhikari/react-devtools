a62e5c59d5a5f91530cdef473bebf61eeebe3b3d
Bikal <bikaladhikari5@gmail.com>
2024-03-28 18:16:30 +1100

    react devtools

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+
+      <Avatar img="https://media1.tenor.com/images/cd37fa49c983ac905df0016fd5b6a2ee/tenor.gif?itemid=13165216" />
+
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Details.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = (props) => {
+  return <p className="info">{props.detailInfo}</p>;
+};
+
+export default Details;
